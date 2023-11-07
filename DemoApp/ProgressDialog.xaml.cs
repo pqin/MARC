a259fd0fcdfca51fd45e2d20498bae5ceb4285c0
Peter Qin <2359042+pqin@users.noreply.github.com>
2023-11-06 19:24:09 -0700

    progress bar moved to modal dialog

---
+++ DemoApp/ProgressDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DemoApp
+{
+    /// <summary>
+    /// Interaction logic for ProgressDialog.xaml
+    /// </summary>
+    public partial class ProgressDialog : Window
+    {
+        public string Message
+        {
+            get { return (string)MessageLabel.Content; }
+            set { MessageLabel.Content = value; }
+        }
+
+        public ProgressDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DialogResult = null;
+        }
+
+        public void ProgressBar_Update(double progress)
+        {
+            ProgressBar.Value = progress;
+        }
+    }
+}
